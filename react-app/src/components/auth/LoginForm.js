ab06acbecad2769383204b05fc208a2c35a95d45
Dane Becker <danebecker790@gmail.com>
2022-03-05 13:55:38 -0600

    made-login-page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -3,12 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 
+import './LoginForm.css'
+
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+  let loginButton;
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -26,19 +29,32 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
+    if (email && password) {
+      loginButton = <button className='login-button' type='submit'>Login</button>
+    } else {
+      loginButton = <button className='login-button-nothing' type='submit'>Login</button>
+    }
+
   if (user) {
     return <Redirect to='/' />;
   }
 
   return (
-    <form onSubmit={onLogin}>
+    <div className='login-signup-main'>
+    <div className='login-signup-article'>
+      <div className='is-image-login'>
+      <img className='login-pic' src='https://i5.walmartimages.com/asr/1c1e8882-6647-4f95-b6da-39183898618c.0b325bb079e2cd662542f5c50717c54a.jpeg?odnHeight=580&odnWidth=580&odnBg=FFFFFF' />
+      </div>
+      <div className='not-image-login'>
+    <form className='main-login-form' onSubmit={onLogin}>
+      <h1 className='logo-form'>InstaOunce</h1>
+      <div className='form'>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
+      <div className='input'>
         <input
           name='email'
           type='text'
@@ -47,8 +63,7 @@ const LoginForm = () => {
           onChange={updateEmail}
         />
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
+      <div className='input'>
         <input
           name='password'
           type='password'
@@ -56,9 +71,13 @@ const LoginForm = () => {
           value={password}
           onChange={updatePassword}
         />
-        <button type='submit'>Login</button>
+        </div>
+          {loginButton}
       </div>
     </form>
+    </div>
+    </div>
+    </div>
   );
 };
 
