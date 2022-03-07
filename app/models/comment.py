e7921540c884ee2a2793b90d3b1c94479dbb4364
Dane Becker <danebecker790@gmail.com>
2022-03-07 11:26:08 -0600

    db should be working

--- app/models/comment.py
+++ app/models/comment.py
@@ -4,14 +4,18 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False)
-    postId = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
     comment = db.Column(db.Text, nullable=False)
+
+    user = db.relationship("User", back_populates="comments")
+    post = db.relationship("Post", back_populates="comments")
+
 
     def to_dict(self):
         return {
             "id": self.id,
-            "userId": self.userId,
-            "postId": self.postId,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
             "comment": self.comment
         }
--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -2,7 +2,7 @@ from app.models import db, Comment
 
 def seed_comments():
     comment1 = Comment(
-        user_id = 3, post_id = 1, comment="Me and my family saw them in Alaska last year!"
+        user_id = 3, post_id = 1, comment="Me and my family saw that in Alaska last year!"
     )
 
     db.session.add(comment1)
