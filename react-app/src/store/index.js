5bebe3459bf1e6433ea17646b667817948260206
Dane Becker <danebecker790@gmail.com>
2022-03-07 12:29:14 -0600

    homepage

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import post from './post'
+import user from './user'
 
 const rootReducer = combineReducers({
   session,
+  post,
+  user
 });
 
 
