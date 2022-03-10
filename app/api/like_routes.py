5b40a6ea23efafd858fdf98b122f8211ae217bf5
brandonvicc <bvicc93@gmail.com>
2022-03-09 19:04:01 -0600

    likes complete

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from flask import Blueprint
-from app.models import Like
+from app.models import Like, Post, db
 
 like_routes = Blueprint('likes', __name__)
 
@@ -7,3 +8,22 @@ like_routes = Blueprint('likes', __name__)
 def likes():
     likes = Like.query.all()
     return {"likes": [like.to_dict() for like in likes]}
+
+
+@like_routes.route('/<int:user_id>/posts/<int:post_id>', methods=['POST'])
+def addLike(post_id, user_id):
+    likes_post = Like.query.filter_by(post_id=post_id).all()
+
+    for like in likes_post:
+        if(user_id == like.user_id):
+            db.session.delete(like)
+            db.session.commit()
+
+            return {"message":'LIKE IS DELETED'}
+
+    new_like = Like(post_id=post_id, user_id=user_id)
+    db.session.add(new_like)
+    db.session.commit()
+
+
+    return {"post": [ like.to_dict() for like in likes_post]}
