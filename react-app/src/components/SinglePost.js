d0ebd4c971d314c961c42561f0267bb03795d2cd
Hamlet <hamlet.villa@gmail.com>
2022-03-11 13:07:19 -0500

    single post work in progress

--- react-app/src/components/SinglePost.js
+++ react-app/src/components/SinglePost.js
@@ -15,20 +15,24 @@ import ViewPostModal from "./ViewPostModal";
 
 const SinglePost = () => {
     const dispatch = useDispatch();
-    const { id } = useParams();
+    const { postId } = useParams();
+
     const userId = useSelector(state => state.session.user?.id);
-    // const userPosts = useSelector((state) => state.post);
-    const postObj = useSelector((state) => state.post);
-    const posts = Object.values(postObj)
-    const singlePost = posts.find(post => id === post.id)
-    console.log('THE POST FROM SINGLE:', posts)
+    const userPostsObj = useSelector((state) => state.post);
+    const posts = Object.values(userPostsObj)
+    console.log(posts)
+    // const postObj = useSelector((state) => state.post[postId]);
+
+    const userPosts = posts.filter(post => postObj.user_id === postId)
+
+    // console.log('SINGLE POST:', postObj)
     const [comment, setComment] = useState('')
     const [commentOptions, setCommentOptions] = useState(false)
     const [viewPost, setViewPost] = useState(false)
 
     useEffect(() => {
-        dispatch(postActions.populatePosts(id));
-        // dispatch(postActions.userPosts(userId));
+        dispatch(postActions.populatePosts());
+        dispatch(postActions.userPosts(userId));
       }, [dispatch]);
 
     const closePost = () => {
@@ -36,39 +40,39 @@ const SinglePost = () => {
         else setViewPost(true);
     };
 
-    return (null
-    //     <>
-    //     <div className="user-profile-post-container">
-    //     <p id="user-profile-post-header">
-    //       <FontAwesomeIcon id="user-profile-post-icon" icon={faClipboard} />{" "}
-    //       Posts
-    //     </p>
-    //     <div className="user-profile-posts-container">
-    //       {userPosts?.map((post) => (
-    //         <div key={post.id} onClick={closePost} className="user-profile-post-card">
-    //           <div className="user-profile-post-info">
-    //             <p>
-    //               <FontAwesomeIcon
-    //                 className="user-profile-post-info-content"
-    //                 icon={faHeart}
-    //               />{" "}
-    //               {post.likes}
-    //             </p>
-    //             <p>
-    //               <FontAwesomeIcon
-    //                 className="user-profile-post-info-content"
-    //                 icon={faComment}
-    //               />{" "}
-    //               {post.comments?.length}
-    //             </p>
-    //           </div>
-    //           <img className="user-profile-post-img" src={post.imageUrl} />
-    //           {viewPost && <ViewPostModal post={post} />}
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    //     </>
+    return (
+        <>
+        <div className="user-profile-post-container">
+        <p id="user-profile-post-header">
+          <FontAwesomeIcon id="user-profile-post-icon" icon={faClipboard} />{" "}
+          Posts
+        </p>
+        <div className="user-profile-posts-container">
+          {userPosts?.map((post) => (
+            <div key={post.id} onClick={closePost} className="user-profile-post-card">
+              <div className="user-profile-post-info">
+                <p>
+                  <FontAwesomeIcon
+                    className="user-profile-post-info-content"
+                    icon={faHeart}
+                  />{" "}
+                  {post.likes}
+                </p>
+                <p>
+                  <FontAwesomeIcon
+                    className="user-profile-post-info-content"
+                    icon={faComment}
+                  />{" "}
+                  {post.comments?.length}
+                </p>
+              </div>
+              <img className="user-profile-post-img" src={post.imageUrl} />
+              {viewPost && <ViewPostModal post={post} />}
+            </div>
+          ))}
+        </div>
+      </div>
+        </>
     )
 }
 
